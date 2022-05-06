b19ad5ab5547b760d4cc7866abd7a7a66541842c
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-05-06 06:36:59 +0200

    Stage 2 progressing

--- File Type Analyzer/task/src/analyzer/Main.kt
+++ File Type Analyzer/task/src/analyzer/Main.kt
@@ -1,25 +1,41 @@
 package analyzer
 
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.math.pow
 
 fun main(args:Array<String>) {
-
-    if (args.size != 3){
-        println("Invalid number of args0")
+    if (args.size != 4){
+        println("Invalid number of args")
     return
     }
 
-    val fileName = args[0];
-    val P = args[1]
-    val R = args[2]
+    if (args[0] !in listOf("--KMP","--naive")){
+        println("First argument should be --KPM or --naive")
+        return
+    }
+
+    val fileName = args[1]
+    val P = args[2]
+    val R = args[3]
 
     val bytes = Files.readAllBytes(Paths.get(fileName)).toList()
     val bytesP = P.toByteArray()
-    var isMatch=false
-    var indexes =bytes.indexesOf(bytesP[0])
 
+    var indexes =bytes.indexesOf(bytesP[0])
+val startTimeNano = System.nanoTime()
+
+    val isMatch = naiveSearch(indexes,bytesP,bytes)
+    val endTimeNano = System.nanoTime()
+    println(if(isMatch) R else "Unknown file type")
+    val nanoToSec = 10.0
+    val takenTimeSeconds = (endTimeNano - startTimeNano) / nanoToSec.pow(-6)
+    println("It took $takenTimeSeconds seconds")
+
+
+}
+fun naiveSearch(indexes: List<Int>, bytesP: ByteArray, bytes: List<Byte>): Boolean {
+    var isMatch=false
     for (index in indexes){
         isMatch = true
         for ( n in 1 .. bytesP.lastIndex){
@@ -31,9 +47,7 @@ fun main(args:Array<String>) {
         if (isMatch)
             break
     }
-    println(if(isMatch) R else "Unknown file type")
-
-
+    return isMatch
 }
 
 fun <E> Iterable<E>.indexesOf(e: E)
